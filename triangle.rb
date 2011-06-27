90bf93938ea3b474c998c50fc77e57fbbbd9ea91
Joseph Morel <jmorel@gmail.com>
2011-06-27 07:39:03 -0400

    More work through koans, including triangle.rb.

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,15 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  if ((a == b) && (b==c))
+    return :equilateral
+  else
+    if ((a == b) || (b==c) || (a == c))
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
